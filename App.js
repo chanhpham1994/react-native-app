011d3c0135bbaeaac22ba28660185b86b4ff185c
Phạm Nguyễn Minh Chánh <51784899+chanhpham1994@users.noreply.github.com>
2022-05-30 21:57:37 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CategoryListItem from "./components/CategoryListItem";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CategoryListItem />
+      <CategoryListItem />
+      <CategoryListItem />
+      <CategoryListItem />
+      <CategoryListItem />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "stretch",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+});
